28ac77f09f79452c5523f2799f9220a20c8ea1f0
Ivan <zhmur.ivan@gmail.com>
2024-04-06 11:15:18 +0300

    added atributes

--- src/Entities/Todo.cs
+++ src/Entities/Todo.cs
@@ -1,11 +1,20 @@
-﻿namespace WindowsFormExam.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WindowsFormExam.Entities;
 
 public class Todo
 {
+    [Key]
+    [Column("id")]
     public int Id { get; set; }
+    [Column("title")]
     public string Title { get; set; }
+    [Column("description")]
     public string Description { get; set; }
+    [Column("created_date")]
     public DateTime CreatedDate { get; set; } = DateTime.Now;
+    [Column("ending_date")]
     public DateTime EndingDate { get; set;}
 
 }
